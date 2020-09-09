db4bec7efe99fc752c508d99450114c7a1c41763
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2020-09-09 04:43:39 -0300

    inserting in databese using fixtures

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use App\Entity\User;
+
+class UserFixtures extends Fixture
+{
+    public function __construct(UserPasswordEncoderInterface $password_encoder)
+    {
+        $this->password_encoder = $password_encoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        foreach ($this->getUserData() as [$email, $roles, $password, $registration, $name,])
+        {
+            $user = new User();
+            $user->setEmail($email);
+            $user->setRoles($roles);
+            $user->setPassword($this->password_encoder->encodePassword($user, $password));
+            $user->setRegistration($registration);
+            $user->setName($name);          
+
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+
+    private function getUserData(): array
+    {
+        return [ 
+            ['jw@symf4.loc', ['ROLE_ADMIN'], 'passw', 23432,'John'],
+            ['jw@symf5.loc', ['ROLE_USER'], 'passw', 23432 ,'John'],
+            ['jw@symf6.loc', ['ROLE_PROFESSOR'], 'passw', 23432 ,'John']
+        ];
+    }
+}
