9b836a410acf3224761c4bbf4e610d58e466db7c
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2020-09-10 01:37:14 -0300

    get users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,28 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+/**
+ * @Route("/user")
+ */
 
 class UserController extends AbstractController
 {
     /**
-     * @Route("/user", name="user")
+     * @Route("/", name="user_index", methods={"GET"})
      */
-    public function index()
+    public function index(): Response
     {
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
         return $this->render('user/user.html.twig', [
-            'controller_name' => 'UserController',
+            'users' => $users,
         ]);
     }
 }
