b85790ce53db560f79229c4380d752151c285850
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2020-09-10 04:46:51 -0300

    create user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,6 +7,9 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Form\UserType;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 
 /**
@@ -29,6 +32,46 @@ class UserController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(UserPasswordEncoderInterface $password_encoder, Request $request)
+    {
+        $user = new User;
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            
+            $user->setEmail($request->request->get('user')['email']);
+            $user->setRoles(['ROLE_USER']);
+            $user->setRegistration($request->request->get('user')['registration']);
+            $user->setName($request->request->get('user')['name']);
+            $password = $password_encoder->encodePassword($user, $request->request->get('user')['password']['first']);
+            $user->setPassword($password);
+            
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->loginUserAutomatically($user, $password);
+
+            return $this->redirectToRoute('user_inde');
+        }
+        return $this->render('user/create_user.html.twig',['form'=>$form->createView()]);
+    }
+
+    private function loginUserAutomatically($user, $password)
+    {
+        $token = new UsernamePasswordToken(
+            $user,
+            $password,
+            'main', // security.yaml
+            $user->getRoles()
+        );
+        $this->get('security.token_storage')->setToken($token);
+        $this->get('session')->set('_security_main',serialize($token));
+    }
 
     /**
      * @Route("/delete-user/{id}", name="delete_user")
