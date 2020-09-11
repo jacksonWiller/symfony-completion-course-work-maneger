9110fa0892dd17ee5a75bbf268f54b52ee8c3398
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2020-09-11 05:24:36 -0300

    make work entity

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -16,15 +16,14 @@ class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager)
     {
-        foreach ($this->getUserData() as [$email, $roles, $password, $registration, $name, $profiler])
-        {
+        foreach ($this->getUserData() as [$email, $roles, $password, $registration, $name, $profiler]) {
             $user = new User();
             $user->setEmail($email);
             $user->setRoles($roles);
             $user->setPassword($this->password_encoder->encodePassword($user, $password));
             $user->setRegistration($registration);
             $user->setName($name);
-            $user->setProfile($profiler);          
+            $user->setProfile($profiler);
 
             $manager->persist($user);
         }
@@ -33,10 +32,10 @@ class UserFixtures extends Fixture
 
     private function getUserData(): array
     {
-        return [ 
-            ['jw@symf4.loc', ['ROLE_ADMIN'], 'passw', 23432,'John','Advisor'],
-            ['jw@symf5.loc', ['ROLE_USER'], 'passw', 23434 ,'John','Student'],
-            ['jw@symf6.loc', ['ROLE_PROFESSOR'], 'passw', 23435 ,'John','Professor']
+        return [
+            ['jw@symf4.loc', ['ROLE_ADMIN'], 'passw', 23432, 'John', 'Advisor'],
+            ['jw@symf5.loc', ['ROLE_USER'], 'passw', 23434, 'John', 'Student'],
+            ['jw@symf6.loc', ['ROLE_PROFESSOR'], 'passw', 23435, 'John', 'Professor']
         ];
     }
 }
---
+++ src/Entity/Work.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WorkRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=WorkRepository::class)
+ */
+class Work
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $area;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $topic;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="student")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $professor;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $student;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getArea(): ?string
+    {
+        return $this->area;
+    }
+
+    public function setArea(string $area): self
+    {
+        $this->area = $area;
+
+        return $this;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getProfessor(): ?User
+    {
+        return $this->professor;
+    }
+
+    public function setProfessor(?User $professor): self
+    {
+        $this->professor = $professor;
+
+        return $this;
+    }
+
+    public function getStudent(): ?User
+    {
+        return $this->student;
+    }
+
+    public function setStudent(?User $student): self
+    {
+        $this->student = $student;
+
+        return $this;
+    }
+}
